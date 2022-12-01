47a5145f7c7e0c3a2db2268b31a9e64df5edcd86
Paul <paul@pbjwalker.com>
2022-11-30 19:59:32 -0600

    Added diff21 warmup exercise from CodingBat

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 # Warmup-1
 # Source: CodingBat
 # https://codingbat.com/prob/p173401
-
+# sleep_in
 # The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
 # This was my solution:
@@ -29,6 +29,47 @@ def sleep_in(weekday, vacation):
 def sleep_in(weekday, vacation):
   return(not weekday or vacation)
   
-# I'm not sure why this works yet. How does "or" work in a return statement?  How does python choose whether to return the boolean value of "not weekday" or to return "vacation"?
+# I'm not sure why this works yet. How does "or" work in a return statement?  How does python choose whether to return the boolean value of "not weekday" or to return "vacation"?  Maybe this is saying, "Return if this statement is True or False: it is not a weekday (not weekday) or vacation".
+# Seems this:
+#  return(vacation)
+# means evaluate the statement, "vacation is True" and return if that statement is True or False
+ 
+
+###################################
+
+# Warmup-1
+# Source: CodingBat
+# https://codingbat.com/prob/p197466
+# diff21
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+def diff21(n):
+  value = abs(21 - n)
+  if n>21:
+    return value*2
+  else:
+    return value
+    
+# My solution worked correctly for all cases.
+# Another solution provided by CodingBat approached it without using abs()
+
+def diff21(n):
+  if n <= 21:
+    return 21 - n
+  else:
+    return (n - 21) * 2
+
+# Either way is just as good, I think.  Looks like it works out the same.
+
+###################################
+
+
+
+
+
+
+
+
+
 
 
