d7218f9fe1f689a3f1b7897cd387da0e3f2fda8f
Paul <paul@pbjwalker.com>
2022-12-12 10:02:23 -0600

    CodingBat Python String-1 problems are complete

--- CodingBat-string_1.py
+++ CodingBat-string_1.py
@@ -105,6 +105,72 @@ def first_half(str):
 #############
 
 # CodingBat
+# https://codingbat.com/prob/p138533
+# without_end
+# Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+# Here's my code:
+
+def without_end(str):
+  return str[1:(len(str)-1)]
+
+# This just takes a slice of the string starting at index position 1.
+# It gets the length of the string, subtracts 1, and that is the end index position for the slice.
+# There's no CodingBat solution for this one.
+
+#############
+
+# CodingBat
+# https://codingbat.com/prob/p194053
+# combo_string
+# Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+# Here's my code:
+
+def combo_string(a, b):
+  if len(a) < len(b):
+    return a+b+a
+  else:
+    return b+a+b
+
+# I got a gold star for this one!
+# It was very straightforward. Easy to follow.
+
+#############
+
+# CodingBat
+# https://codingbat.com/prob/p127703
+# non_start
+# Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+# Here's my code:
+
+def non_start(a, b):
+  return a[1:] + b[1:]
+
+# I just concatenated the slices of each string starting at index position 1
+# There's no CodingBat solution
+
+#############
+
+# CodingBat
+# https://codingbat.com/prob/p160545
+# left2
+# Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+# Here's my code:
+
+def left2(str):
+  return str[2:] + str[:2]
+
+# I got a gold star for this one!
+# At first, I sliced the string wrong, but it was an easy fix.
+# My solution was exactly like the CodingBat solution
+
+
+
+# End of String-1 practice problems
+#############
+
+
+
+
 
 
 
