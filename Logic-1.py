ba3aaf4effaf624db3240cc8008853a55674537b
Paul <paul@pbjwalker.com>
2022-12-18 23:04:08 -0600

    CodingBat Logic-1 practice date-fashion

--- Logic-1.py
+++ Logic-1.py
@@ -22,6 +22,39 @@ def cigar_party(cigars, is_weekend):
   else:
     return (cigars >= 40 and cigars <= 60)
 
+############
+# CodingBat
+# Logic-1 practice problems
+# https://codingbat.com/prob/p129125
+# date_fashion
+# You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+# Here's my code:
+
+def date_fashion(you,date):
+  if (you >= 8) or (date >= 8):
+    return 2
+  elif (you <= 2) or (date <= 2):
+    return 0
+  else:
+    return 1
+
+# My first run had an error.
+# It didn't take into account the exception.
+# Just needed to make the check on the exception first.
+# Here's the fixed code:
+
+def date_fashion(you,date):
+  if (you <= 2) or (date <= 2):
+    return 0
+  elif (you >= 8) or (date >= 8):
+    return 2
+  else:
+    return 1
+
+# My code is identical to the solution.
+
+
+############
 # CodingBat
 # Logic-1 practice problems
 
