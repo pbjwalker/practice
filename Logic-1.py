3b31b50adcaf0232b0fa776c6b3b523517a90abc
Paul <paul@pbjwalker.com>
2022-12-19 20:46:11 -0600

    CodingBat Logic-1 practice problems: squirrel_play, and caught_speeding

--- Logic-1.py
+++ Logic-1.py
@@ -57,6 +57,56 @@ def date_fashion(you,date):
 ############
 # CodingBat
 # Logic-1 practice problems
+# https://codingbat.com/prob/p135815
+# squirrel_play
+# The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+# Here's my code:
+
+def squirrel_play(temp, is_summer):
+  if is_summer:
+    return (temp >= 60) and (temp <= 100)
+  else:
+    return (temp >= 60) and (temp <= 90)
+    
+# And it worked perfect!  I got a gold star for this one!
+# Granted it's a simple problem,
+# but I like working with logic.
+# No CodingBat solution provided.
+
+############
+# CodingBat
+# Logic-1 practice problems
+# https://codingbat.com/prob/p137202
+# caught_speeding
+# You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+# Here's my code:
+
+def caught_speeding(speed, is_birthday):
+  if is_birthday:
+    if speed >= 86:
+      return 2
+    elif speed <= 65:
+      return 0
+    else:
+      return 1
+  else:
+    if speed >= 81:
+      return 2
+    elif speed <= 60:
+      return 0
+    else:
+      return 1   
+      
+# That worked!
+# I think that's a lot of if-elif-else statements.
+# Maybe I'll learn a better way of doing that soon.
+
+############
+# CodingBat
+# Logic-1 practice problems
+
+
+
 
 
 
